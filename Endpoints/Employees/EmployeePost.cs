85ba86d5bd759f6e6b5201966e51cd1c5d4684c3
Francisco Lima <franciscojml@gmail.com>
2022-10-04 16:21:48 -0300

    Initial Identity

---
+++ Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IWantApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(EmployeeRequest request, UserManager<IdentityUser> userManager)
+    {
+        var user = new IdentityUser { UserName = request.Email, Email = request.Email};
+        var result = userManager.CreateAsync(user).Result;
+
+        if (!result.Succeeded)
+            return Results.BadRequest(result.Errors.First());
+        
+        return Results.Created($"/employees/{user.Id}", user.Id);
+    }
+}
--- Infra/Data/ApplicationDbContext.cs
+++ Infra/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Flunt.Notifications;
 using IWantApp.Domain.Products;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection.Emit;
 
 namespace IWantApp.Infra.Data;
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -16,6 +18,7 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
         builder.Ignore<Notification>();
         builder.Entity<Product>().Property(p => p.Name).IsRequired();
         builder.Entity<Product>().Property(p => p.Description).HasMaxLength(255);
